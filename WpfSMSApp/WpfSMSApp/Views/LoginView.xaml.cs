31261411f09414bc3b1732aa51285815beb553d3
ieyeppo <ieyeppo@gmail.com>
2021-03-25 17:20:02 +0900

    03-25커밋

---
+++ WpfSMSApp/WpfSMSApp/Views/LoginView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace WpfSMSApp.Views
+{
+    /// <summary>
+    /// LoginView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginView : MetroWindow
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
